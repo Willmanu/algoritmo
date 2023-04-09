8ba5308111b9c2dfd46e652b9c03e8ca69fc3fb2
Willmanu <pamanuwill@gmail.com>
2023-04-09 20:08:08 -0300

    Falando do algoritimo de pesquisa binária

--- 1buscalinear/2pesquisabinaria.rb
+++ 1buscalinear/2pesquisabinaria.rb
@@ -1,11 +1,28 @@
 =begin
 
+                                      Pesquisa Binária
  A Pesquisa Binária é um algoritmo de busca eficiente que opera em listas ordenadas
  O objetivo da Pesquisa Binária é encontrar a posição de um determinado elemento em uma lista
  ordenada, dividindo repetidamente a lista em duas metades(por isso seu nome é binária) e
  comparando o elemento desejado com o elemento no meio da lista
 
   A complexidade da Pesquisa Binária é O(log n), onde n é o número de elementos na lista.
+  O termo "O(Long n)" é uma abreviação para a notação "Big O" usada em análise de algoritmos
+   para descrever a complexidade de tempo de um algoritmo.
+ A notação "Big O" é usada para expressar a ordem de crescimento do tempo de execução de um
+ algoritmo em relação ao tamanho de entrada do problema
+ Ela fornece uma maneira de expressar o pior caso do tempo de execução de um algoritmo, em termos
+ de uma função matemática que é uma aproximação assintótica (ou seja, para tamanhos de entrada
+ grandes) do tempo de execução.
+
+ Por exemplo, se um algoritmo tem complexidade O(Long n), isso significa que, se a entrada for
+ multiplicada por 10, o tempo de execução do algoritmo aumentará em um fator próximo a 1, ou
+ seja, o tempo de execução aumentará muito pouco em comparação com a mudança na entrada
+
+ Portanto, um algoritmo com complexidade O(Long n) é considerado muito eficiente para grandes
+ conjuntos de dados, especialmente quando comparado com algoritmos com complexidade O(n) ou
+ superior.
+
 =end
 
 array = [10, 20, 30, 50, 60, 80, 110, 130, 140, 170]
